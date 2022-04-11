6a3b46b91dc133a07bbca96127d62885de6e2585
Praveen Kumar <32730074+pkm141194@users.noreply.github.com>
2022-04-11 09:01:41 +0530

    Create Read CSV , filter col name and save into excel.py

---
+++ CSV/Read CSV , filter col name and save into excel.py
@@ -0,0 +1,31 @@
+1.1 Sequence (Sequence)			
+	Private = False		
+	Variables		
+		num(String)	
+		getfile(String[])	
+		Jsonobject(String)	
+		jobject(JObject)	
+		jsonname(String)	
+		csvReader(DataTable)	
+		input(DataTable)	
+		ouput(DataTable)	
+	Activities		
+		1.2 Read CSV (ReadCsvFile)	
+			FilePath = C:\Users\pkm14\OneDrive\Desktop\Excel format.csv
+			DataTable = input
+			Delimiter = Comma
+			Has headers = True
+			Private = False
+		1.2 Filter Data Table (FilterDataTable)	
+			DataTable = input
+			SelectColumnsMode = Keep
+			FilterRowsMode = Keep
+			DataTable = ouput
+			Private = False
+		1.2 Write Range (WriteRange)	
+			StartingCell = A1
+			DataTable = ouput
+			AddHeaders = False
+			SheetName = Sheet1
+			Workbook path = C:\Users\pkm14\OneDrive\Desktop\result1.xlsx
+			Private = False
